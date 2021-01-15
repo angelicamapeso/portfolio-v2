ddac93b5fe80c955f710fa5597174123f9738d26
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-14 23:19:55 -0500

    Add jumbotron to homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -17,6 +17,32 @@ export default function Home() {
           content="Hi, I'm Angelica Mapeso, an aspiring full-stack developer."
         />
       </Helmet>
+      <main class="d-flex flex-column flex-grow-1">
+        <section id="hero">
+          <Container>
+            <Row>
+              <Col sm>
+                <Jumbotron className="px-0">
+                  <h1 class="display-4 left-border-title">
+                    <span id="title-hi" class="display-3">
+                      Hi,{" "}
+                    </span>
+                    My name is Angelica&nbsp;Mapeso.
+                  </h1>
+                  <div class="left-border-line">
+                    <p class="mb-5 h3 font-weight-light">
+                      I'm an aspiring web developer and web designer.
+                    </p>
+                    <Link to="/about">
+                      <Button variant="dark">Learn more about me</Button>
+                    </Link>
+                  </div>
+                </Jumbotron>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      </main>
     </>
   );
 }
