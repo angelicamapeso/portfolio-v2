d522415446264cebd6133ad612844fcf82072b84
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-14 22:51:44 -0500

    Implement Router for multiple pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Test</h1>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <h1>Home</h1>
+          </Route>
+          <Route exact path="/portfolio">
+            <h1>Portfolio</h1>
+          </Route>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
+          <Route path="/contact">
+            <h1>Contact</h1>
+          </Route>
+          <Route path="/portfolio/:name">
+            <h1>Portfolio Project</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
