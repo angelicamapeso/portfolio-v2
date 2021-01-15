e852c2734c5e25302ccfe97f1ed77680bbf58be1
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-15 18:23:59 -0500

    Hook up projPage to routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
@@ -17,9 +18,7 @@ function App() {
           <Route path="/contact">
             <h1>Contact</h1>
           </Route>
-          <Route path="/portfolio/:name">
-            <h1>Portfolio Project</h1>
-          </Route>
+          <Route path="/portfolio/:name" component={ProjectPage}></Route>
         </Switch>
       </Router>
     </>
