c005b363dd7a56ae9ac91ecc88f67b15696ab4d3
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-15 15:31:49 -0500

    Add ProjectCard component

---
+++ src/components/ProjectCard/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
+function ProjectCard(props) {
+  return (
+    <Col xs={12} sm={6} lg={4}>
+      <Card>
+        <a href="./projects/eat-local.html" aria-label="Eat local website">
+          <Card.Img
+            variant="top"
+            src={props.project.img.icon}
+            className="mb-3"
+            alt={props.project.title}
+          />
+        </a>
+        <Card.Body className="left-border-line">
+          <Card.Title className="mb-0">{props.project.title}</Card.Title>
+          <Card.Text>{props.project.tagline}</Card.Text>
+          <div className="mt-auto d-flex flex-column justify-content-end">
+            <p className="mb-0">
+              <a href={props.project.url} className="h5 icon-link">
+                <i className="fa fa-link" aria-hidden="true"></i>Visit site
+              </a>
+            </p>
+            <p className="mb-0">
+              <a href={props.project.repo} className="h5 icon-link">
+                <i className="fa fa-github" aria-hidden="true"></i>View source
+                code
+              </a>
+            </p>
+          </div>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
+
+export default ProjectCard;
