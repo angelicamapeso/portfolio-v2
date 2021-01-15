a5dd9d8fa002ffba36ea2f0cc7524838d2ed2550
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-14 23:19:42 -0500

    Add helmet, homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Helmet>
+        <title>Angelica Mapeso | Home</title>
+        <meta
+          name="description"
+          content="Hi, I'm Angelica Mapeso, an aspiring full-stack developer."
+        />
+      </Helmet>
+    </>
+  );
 }
