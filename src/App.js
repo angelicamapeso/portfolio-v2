ef0e0fb638cafa9dd720b12643dd9a5331f36bb6
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-14 22:54:25 -0500

    Create home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Switch>
-          <Route exact path="/">
-            <h1>Home</h1>
-          </Route>
+          <Route exact path="/" component={Home} />
           <Route exact path="/portfolio">
             <h1>Portfolio</h1>
           </Route>
