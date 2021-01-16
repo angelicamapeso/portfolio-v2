b74cf5507c987724c4baf2da5cc564589c10e472
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-15 20:32:04 -0500

    Add option for list item to not have link

--- src/components/LinkList/index.js
+++ src/components/LinkList/index.js
@@ -5,9 +5,13 @@ function LinkList(props) {
     <ul class="text-list">
       {props.items.map(item => (
         <li>
-          <a className="icon-link" href={item.link}>
-            {item.name}
-          </a>
+          {item.link ? (
+            <a className="icon-link" href={item.link}>
+              {item.name}
+            </a>
+          ) : (
+            <p className="d-inline-block mb-0">{item.name}</p>
+          )}
         </li>
       ))}
     </ul>
