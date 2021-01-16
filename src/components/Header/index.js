8a048dabc665f6da873034046498ed68fb4cf252
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-16 16:51:51 -0500

    Add header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../img/logo_maroon.svg";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+function Header() {
+  return (
+    <header>
+      <Navbar expand="lg" bg="light" className="mb-4">
+        <Navbar.Brand>
+          <Link to="/">
+            <img src={logo} width="50" height="50" alt="Home" loading="lazy" />
+          </Link>
+        </Navbar.Brand>
+        <Navbar.Toggle id="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="align-items-end">
+            <li className="nav-item">
+              <Link to="/portfolio" className="nav-link h5 mb-0">
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link h5 mb-0">
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link h5 mb-0">
+                Contact
+              </Link>
+            </li>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
