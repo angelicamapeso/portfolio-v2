f9d904914e99fabe0ed7f62d93976b5dd59bfbe5
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-16 17:10:14 -0500

    Link Portfolio page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import ProjectPage from "./pages/ProjectPage";
@@ -13,9 +14,7 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/portfolio">
-            <h1>Portfolio</h1>
-          </Route>
+          <Route exact path="/portfolio" component={Portfolio} />
           <Route path="/about" component={About} />
           <Route path="/contact" component={Contact} />
           <Route path="/portfolio/:name" component={ProjectPage} />
