19e2ee1b64668f7e719bb8a7fb373164f6c45d48
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-16 15:27:27 -0500

    Write skills ot new file

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -6,58 +6,7 @@ import Col from "react-bootstrap/Col";
 import LinkList from "../../components/LinkList";
 
 import resume from "./angelicaMapeso_resume2021.pdf";
-
-const skills = [
-  {
-    icon: "fa fa-desktop",
-    title: "Front-End Skills",
-    items: [
-      {
-        name: "HTML",
-      },
-      {
-        name: "CSS",
-      },
-      {
-        name: "JavaScript",
-      },
-      {
-        name: "Bootstrap",
-        link: "https://getbootstrap.com/",
-      },
-      {
-        name: "Handlebars",
-        link: "https://handlebarsjs.com/",
-      },
-      {
-        name: "React.js",
-        link: "https://reactjs.org/",
-      },
-    ],
-  },
-  {
-    icon: "fa fa-desktop",
-    title: "Back-End Skills",
-    items: [
-      {
-        name: "Node.js",
-        link: "https://nodejs.org/en/",
-      },
-      {
-        name: "Express",
-        link: "https://expressjs.com/",
-      },
-      {
-        name: "MySQL",
-        link: "https://www.mysql.com/",
-      },
-      {
-        name: "MongoDB",
-        link: "https://www.mongodb.com/",
-      },
-    ],
-  },
-];
+import { skills } from "./skills.js";
 
 function About() {
   return (
---
+++ src/pages/About/skills.js
@@ -0,0 +1,51 @@
+export const skills = [
+  {
+    icon: "fa fa-desktop",
+    title: "Front-End Skills",
+    items: [
+      {
+        name: "HTML",
+      },
+      {
+        name: "CSS",
+      },
+      {
+        name: "JavaScript",
+      },
+      {
+        name: "Bootstrap",
+        link: "https://getbootstrap.com/",
+      },
+      {
+        name: "Handlebars",
+        link: "https://handlebarsjs.com/",
+      },
+      {
+        name: "React.js",
+        link: "https://reactjs.org/",
+      },
+    ],
+  },
+  {
+    icon: "fa fa-desktop",
+    title: "Back-End Skills",
+    items: [
+      {
+        name: "Node.js",
+        link: "https://nodejs.org/en/",
+      },
+      {
+        name: "Express",
+        link: "https://expressjs.com/",
+      },
+      {
+        name: "MySQL",
+        link: "https://www.mysql.com/",
+      },
+      {
+        name: "MongoDB",
+        link: "https://www.mongodb.com/",
+      },
+    ],
+  },
+];
