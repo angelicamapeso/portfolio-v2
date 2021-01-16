948a15ca4d53af98710829bac572e79e81695357
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2021-01-15 20:34:36 -0500

    Add helmet to about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Helmet from "react-helmet";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -60,67 +61,73 @@ const skills = [
 
 function About() {
   return (
-    <section id="portfolio" className="pb-4">
-      <Container>
-        <Row>
-          <Col>
-            <h1 className="mb-4 display-4 left-border-title">About</h1>
-            <div className="left-border-line">
-              <Row>
-                <Col xs={12} md={4} className="order-md-1 mb-5">
-                  <div id="about-img"></div>
-                </Col>
-                <Col xd={12} md={8}>
-                  <p className="lead">
-                    Currently a student at Carleton University in the
-                    Interactive Multimedia Program and the Carleton Coding
-                    Bootcamp.
-                  </p>
-                  <p>
-                    I have a solid foundation in designing for multimedia
-                    (animation, web design, UI/UX) through the Interactive
-                    Multimedia and Design program at Carleton University, and am
-                    currently learning the MERN stack through the Carleton
-                    University Full-Stack coding bootcamp.
-                  </p>
-                  <p className="mb-5">
-                    I am passionate about creating beautiful and functional user
-                    interfaces and user experiences. Equipped with knowledge in
-                    graphic design, UI/UX design, web development and
-                    collaborative skills, I am hoping to eventually work as a
-                    full-stack developer.
-                  </p>
-                </Col>
-              </Row>
-              <Row className="pb-3">
-                {skills.map(skill => (
-                  <Col xs={12} md className="mb-3">
-                    <h2 className="font-weight-light">
-                      <i
-                        className={skill.icon + " mr-3"}
-                        aria-hidden="true"
-                      ></i>
-                      {skill.title}
-                    </h2>
-                    <LinkList items={skill.items} />
+    <>
+      <Helmet>
+        <title>Angelica Mapeso | About</title>
+        <meta name="description" content="Get to know me a little bit better" />
+      </Helmet>
+      <section id="portfolio" className="pb-4">
+        <Container>
+          <Row>
+            <Col>
+              <h1 className="mb-4 display-4 left-border-title">About</h1>
+              <div className="left-border-line">
+                <Row>
+                  <Col xs={12} md={4} className="order-md-1 mb-5">
+                    <div id="about-img"></div>
                   </Col>
-                ))}
-              </Row>
-              <p className="mb-5">
-                <a href={resume} className="icon-link">
-                  <i className="fa fa-file-pdf-o" aria-hidden="true"></i>View my
-                  Resume for more information
+                  <Col xd={12} md={8}>
+                    <p className="lead">
+                      Currently a student at Carleton University in the
+                      Interactive Multimedia Program and the Carleton Coding
+                      Bootcamp.
+                    </p>
+                    <p>
+                      I have a solid foundation in designing for multimedia
+                      (animation, web design, UI/UX) through the Interactive
+                      Multimedia and Design program at Carleton University, and
+                      am currently learning the MERN stack through the Carleton
+                      University Full-Stack coding bootcamp.
+                    </p>
+                    <p className="mb-5">
+                      I am passionate about creating beautiful and functional
+                      user interfaces and user experiences. Equipped with
+                      knowledge in graphic design, UI/UX design, web development
+                      and collaborative skills, I am hoping to eventually work
+                      as a full-stack developer.
+                    </p>
+                  </Col>
+                </Row>
+                <Row className="pb-3">
+                  {skills.map(skill => (
+                    <Col xs={12} md className="mb-3">
+                      <h2 className="font-weight-light">
+                        <i
+                          className={skill.icon + " mr-3"}
+                          aria-hidden="true"
+                        ></i>
+                        {skill.title}
+                      </h2>
+                      <LinkList items={skill.items} />
+                    </Col>
+                  ))}
+                </Row>
+                <p className="mb-5">
+                  <a href={resume} className="icon-link">
+                    <i className="fa fa-file-pdf-o" aria-hidden="true"></i>View
+                    my Resume for more information
+                  </a>
+                </p>
+                <a className="btn btn-dark" href="contact.html" role="button">
+                  Let's get in touch{" "}
+                  <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
                 </a>
-              </p>
-              <a className="btn btn-dark" href="contact.html" role="button">
-                Let's get in touch{" "}
-                <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
-              </a>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   );
 }
 
