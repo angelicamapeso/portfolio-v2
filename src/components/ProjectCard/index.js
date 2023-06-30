77dc3ee5387543a8665908c327002d08db77d033
Angelica Mapeso <angelica.mapeso@gmail.com>
2023-06-30 01:32:38 -0400

    add animation to project card and make entire card clickable

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -5,47 +5,43 @@ import Card from "react-bootstrap/Card";
 import { ReactComponent as Arrow } from "../../img/arrow_down_white.svg";
 import "./style.scss";
 
-function ProjectCard({ project }) {
+function ProjectCard({ project, isPreview }) {
   return (
     <Col xs={12} sm={6} lg={4}>
       <Card>
-        <Card.Title className="project-title-line left-border-line">
-          <span className="project-id">{project.id}</span>
-          <span className="circle-separator" aria-hidden="true"></span>
-          <span className="project-title">{project.title}</span>
-        </Card.Title>
-        <Card.Img
-          variant="top"
-          src={project.img}
-          alt={project.title}
-        />
-        <Card.Body>
-          <Card.Text>{project.tagline}</Card.Text>
-          <div className="project-tag-list">
-            {project.tags?.map((tag, index) =>
-              <p
-                className="project-tag"
-                key={index}
-              >
-                {`#${tag}`}
+        <Link
+          className="image-link"
+          to={"/portfolio" + project.infoPage}
+          aria-label={`Learn more about ${project.title}`}
+        >
+          <Card.Title className="project-title-line left-border-line">
+            <span className="project-id">{project.id}</span>
+            <span className="circle-separator" aria-hidden="true"></span>
+            <span className="project-title">{project.title}</span>
+          </Card.Title>
+          <Card.Img variant="top" src={project.img} alt={project.title} />
+          <Card.Body>
+            <Card.Text>{project.tagline}</Card.Text>
+            <div className="project-tag-list">
+              {project.tags?.map((tag, index) => (
+                <p className="project-tag" key={index}>
+                  {`#${tag}`}
+                </p>
+              ))}
+            </div>
+            <div className="project-card-footer">
+              {project.repo ? (
+                <p className="mr-2 on-github dark-maroon-bubble">
+                  <i className="fab fa-github mr-2" aria-hidden="true"></i>
+                  On GitHub
+                </p>
+              ) : null}
+              <p className="project-about">
+                Learn more <Arrow className="more-arrow" />
               </p>
-            )}
-          </div>
-          <div className="project-navigation">
-            {project.repo ?
-              <a className="dark-green-link" href={project.repo}>
-                <i className="fab fa-github mr-2" aria-hidden="true"></i>Source Code
-              </a> : null
-            }
-            <Link
-              className="project-about"
-              to={"/portfolio" + project.infoPage}
-              aria-label={`Learn more about ${project.title}`}
-            >
-              Learn more <Arrow className="more-arrow" />
-            </Link>
-          </div>
-        </Card.Body>
+            </div>
+          </Card.Body>
+        </Link>
       </Card>
     </Col>
   );
