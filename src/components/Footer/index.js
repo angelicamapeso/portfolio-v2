3771f37e5472828568cedd8763857a515f64fabb
Angelica Mapeso <angelica.mapeso@gmail.com>
2023-07-06 13:07:15 -0400

    style footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,29 +3,24 @@ import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import "./style.css";
+import "./style.scss";
 
 function Footer() {
   return (
     <footer className="footer mt-auto py-3">
-      <Container>
+      <Container fluid>
         <Row>
           <Col>
-            <Link to="/contact" className="mr-3 mb-0 h5">
-              Contact
-            </Link>
-          </Col>
-          <Col xs="auto">
             <a
               href="https://github.com/angelicamapeso"
-              className="mr-3 h4"
+              className="mr-4 image-link"
               aria-label="Github"
             >
               <i className="fab fa-github" aria-hidden="true"></i>
             </a>
             <a
               href="https://www.linkedin.com/in/angelica-mapeso"
-              className="mr-3 h4"
+              className="image-link"
               aria-label="LinkedIn"
             >
               <i className="fab fa-linkedin" aria-hidden="true"></i>
